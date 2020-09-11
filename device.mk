a35e8f55bac429fc72858c254fbfde25e4d8afd2
Harshit Jain <god@hyper-labs.tech>
2020-09-11 21:51:01 +0530

    X2: remove boot jars for now

--- device.mk
+++ device.mk
@@ -176,8 +176,8 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+#PRODUCT_BOOT_JARS += \
+#    telephony-ext
 
 # TextClassifier
 PRODUCT_PACKAGES += \
@@ -195,8 +195,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libnl
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#    WfdCommon
 
 # Lights
 PRODUCT_PACKAGES += \
